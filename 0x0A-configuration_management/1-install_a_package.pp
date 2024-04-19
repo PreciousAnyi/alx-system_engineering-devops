27ee20d017e30ed4cb0811a0658d99278a182120
preciousanyi <jkpresh@gmail.com>
2024-04-19 15:57:24 -0700

    Modified file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
 # Install Flask with pip
+package { 'python3':
+  ensure => '3.8.10',
+}
+
+package { ['python3', 'python3-pip', 'python3-werkzeug']:
+  ensure => installed,
+}
 
 package {'Flask':
   ensure   => '2.1.0',
