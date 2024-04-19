bfd18820b993f51b4551fda81de0f077f92e2bb3
Presh <jkpresh@gmail.com>
2024-04-19 16:23:08 -0700

    Modified file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,22 @@
-# Install Flask with pip
-package { 'python3':
-  ensure => '3.8.10',
+# install the flask package with a specific version and provider
+
+# install python package of version 3.8.10
+package { 'python3.8':
+  ensure  => '3.8.10',
 }
 
-package { 'werkzeug':
-  ensure => '2.1.1',
-}
-
+# ensure pip3 is install
 package { 'python3-pip':
-  ensure => installed,
+  ensure  => present,
+}
+# ensure pip is installed before installing flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
 
-package {'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# ensure flask is installed before installing werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
 }
