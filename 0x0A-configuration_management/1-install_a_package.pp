7dee11fd2636c035e9be9f53a75a19b5ea0864c7
preciousanyi <jkpresh@gmail.com>
2024-04-19 16:02:51 -0700

    Modified file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,7 +3,11 @@ package { 'python3':
   ensure => '3.8.10',
 }
 
-package { ['python3', 'python3-pip', 'python3-werkzeug']:
+package { 'werkzeug':
+  ensure => '2.1.1',
+}
+
+package { 'python3-pip':
   ensure => installed,
 }
 
